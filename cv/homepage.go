7485267a6202ccf945de6ecf38ff337f2033f49b
bbantu <brianbaraza40@gmail.com.com>
2024-10-29 15:53:08 +0300

    placed index.html in a template folder

--- cv/homepage.go
+++ cv/homepage.go
@@ -6,7 +6,7 @@ import (
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("index.html") // Make sure "index.html" is in the same directory
+	tmpl, err := template.ParseFiles("templates/index.html") // Make sure "index.html" is in the same directory
 	if err != nil {
 		http.Error(w, "Unable to load template", http.StatusInternalServerError)
 		return
--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	// Start the server
-	log.Println("Starting server on :8080")
+	log.Println("Starting server on http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("Server failed:", err)
